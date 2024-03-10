7740af312b3308f8f12215b16afe74288f0dbae1
BernardinoHero <bernardino.behero@gmail.com>
2024-03-09 23:01:38 -0600

    modificación de estilos del paso 1 y paso 2

--- src/components/admision/AdmisionProcess.jsx
+++ src/components/admision/AdmisionProcess.jsx
@@ -1,6 +1,6 @@
-import React, { useState } from "react";
+import React, {useState} from "react";
 
-const AdmisionProcess = ({ processType }) => {
+const AdmisionProcess = ({processType}) => {
     const [activeStep, setActiveStep] = useState(0);
 
     const handleStepClick = (step) => {
@@ -10,30 +10,65 @@ const AdmisionProcess = ({ processType }) => {
     const getStepsData = () => {
         const licenciaturaSteps = [
             {
-                title: "Verificar fechas",
+                title: (<h1 className="mt-4 text-2xl md:text-3xl lg:text-4xl font-bold">Fechas importantes</h1>),
                 description: {
-                    tokenDelivery: "Entrega de fichas: Del 16 de febrero al 26 de junio del 2024.",
-                    admissionTest:"Examen de admisión: 25 de mayo y 01 de julio del año 2024.",
-                    registrationForThePreparatoryCourse:"Inscripción al curso propedéutico: Del 15 al 26 de julio del 2024.",
-                    durationOfThePreparatoryCourse:"Duración del curso propedéutico: Del 29 de julio al 20 de septiembre del año 2024.",
+                    tokenDelivery: (<p className="text-lg md:text-2xl lg:text-3xl flex flex-col"><strong
+                        className='text-lg md:text-2xl lg:text-3xl'>Entrega de fichas:</strong> Del 16 de febrero al 26
+                        de junio del 2024.</p>),
+                    admissionTest: (<p className="text-lg md:text-2xl lg:text-3xl flex flex-col"><strong
+                        className='text-lg md:text-2xl lg:text-3xl'>Examen de admisión:</strong> 25 de mayo y 01 de
+                        julio del año 2024.</p>),
+                    registrationForThePreparatoryCourse: (
+                        <p className="text-lg md:text-2xl lg:text-3xl flex flex-col"><strong
+                            className='text-lg md:text-2xl lg:text-3xl'>Inscripción al curso propedéutico:</strong> Del
+                            15
+                            al 26 de julio del 2024.</p>),
+                    durationOfThePreparatoryCourse: (
+                        <p className="text-lg md:text-2xl lg:text-3xl flex flex-col"><strong
+                            className='text-lg md:text-2xl lg:text-3xl'>Duración del curso propedéutico:</strong> Del 29
+                            de
+                            julio al 20 de septiembre del año 2024.</p>),
                 }
             },
             {
-                title: "Recolección de documentos",
-                description:{
-                    1:"1 copia del acta de nacimiento.",
-                    2:"1 copia del certificado de secundaria.",
-                    3:"1 copia del certificado de bachillerato o constancia de estudios con la tira de materias de 1° al 5° semestre.",
-                    4:"1 copia del CURP.",
-                    5:"6 fotografías tamaño infantil b/n en papel mate.",
-                    6:"Original del comprobante de pago de ficha (se obtiene en el siguiente paso).",
+                title: (<h1 className="mt4 text-2xl md:text-3xl lg:text-4xl font-bold">Recolección de documentos</h1>),
+                description: {
+                    1: (<p className="text-lg md:text-2xl lg:text-3xl">1 copia del acta de nacimiento.</p>),
+                    2: (<p className="text-lg md:text-2xl lg:text-3xl">1 copia del certificado de secundaria.</p>),
+                    3: (<p className="text-lg md:text-2xl lg:text-3xl">1 copia del certificado de bachillerato o
+                        constancia de estudios con la tira de materias de 1° al 5° semestre.</p>),
+                    4: (<p className="text-lg md:text-2xl lg:text-3xl">1 copia de la CURP.</p>),
+                    5: (<p className="text-lg md:text-2xl lg:text-3xl">6 fotografías tamaño infantil b/n en papel
+                        mate.</p>),
+                    6: (<p className="text-lg md:text-2xl lg:text-3xl flex flex-col text-center">Original del
+                        comprobante de pago de ficha
+                        <strong>
+                            (Realiza el pago
+                            <a className="pl-2 cursor-pointer text-blue-600 text-2xl md:text-3xl"
+                               href="https://siox.finanzasoaxaca.gob.mx/pagos"
+                               target="_blank"
+                               rel="noopener noreferrer"
+                            >
+                                en este enlace
+                            </a>)
+                        </strong>.
+                    </p>),
                 }
             },
             {
                 title: "Ficha escolar",
-                description:{
-                    1:"Pago de ficha escolar:",
-                    2:"https://siox.finanzasoaxaca.gob.mx/pagos",
+                description: {
+                    1: "Pago de ficha escolar:",
+                    2: (
+                        <a
+                            className="inline-block text-blue-500 sm:text-sm md:text-xl lg:text-lg xl:text-xl"
+                            href="https://siox.finanzasoaxaca.gob.mx/pagos"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            Finanzas Oaxaca
+                        </a>
+                    ),
                 }
             },
             {
@@ -53,7 +88,7 @@ const AdmisionProcess = ({ processType }) => {
 
     return (
         <div className="bg-unsij_white h-full pb-12 rounded-b-lg">
-            <div className="w-[90%] mx-auto px-4 sm:px-6 lg:px-8 h-full flex flex-col justify-center">
+            <div className="w-full sm:w-[90%] mx-auto px-4 sm:px-6 lg:px-8 h-full flex flex-col justify-center">
                 <div className="flex mt-4 flex-col lg:flex-row justify-center items-center gap-8">
                     <span className="text-sm leading-7 text-gray-500 font-regular">
                         Colegiatura {" "} <strong className="font-bold text-xl md:text-3xl">GRATIS</strong>
@@ -62,11 +97,11 @@ const AdmisionProcess = ({ processType }) => {
                         Costo de ficha {" "} <strong className="font-bold text-xl md:text-3xl">$239 MXN</strong>
                     </span>
                 </div>
-                <div className="flex justify-around mt-8">
+                <div className="flex justify-around mt-8" style={{fontSize: "0.8rem"}}>
                     {stepsData.map((step, index) => (
                         <div key={`${processType}-${index}`} className="cursor-pointer">
                             <div
-                                className={`flex items-center justify-center h-10 md:h-20 w-10 md:w-20 rounded-full bg-unsij_secod text-3xl text-white font-semibold ${
+                                className={`flex items-center justify-center h-10 w-10 md:h-16 md:w-16 lg:w-24 lg:h-24 rounded-full bg-unsij_secod text-3xl text-white font-semibold ${
                                     activeStep === index ? "bg-unsij_sixth" : ""
                                 }`}
                                 onClick={() => handleStepClick(index)}
